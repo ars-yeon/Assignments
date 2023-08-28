f7b6262cc1e473c8954c6472861bd6e372832b90
ars-yeon <zosel7301@gmail.com>
2023-08-28 19:56:18 +0900

    [FEAT] : (ContactAdapter) Handle item view click event
    - Initiate call action with the phone number of the respective position
    
    Related to #6

--- ClassLevels/ContactListApp/app/src/main/java/com/example/contactlistapp/ContactAdapter.kt
+++ ClassLevels/ContactListApp/app/src/main/java/com/example/contactlistapp/ContactAdapter.kt
@@ -1,7 +1,10 @@
 package com.example.contactlistapp
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contactlistapp.databinding.ContactItemBinding
@@ -15,6 +18,13 @@ class ContactAdapter(private val mItems: List<Contact>, internal val context: Co
         return Holder(binding)
     }
 
+    // 아이템 클릭 이벤트 인터페이스
+    interface ItemClick {
+        fun onClick(view: View, position: Int)
+    }
+
+    var itemClick: ItemClick? = null  // 아이템 클릭 리스너
+
     // 뷰 홀더에 데이터 바인딩
     override fun onBindViewHolder(holder: ContactAdapter.Holder, position: Int) {
         val item = mItems[position] // 현재 위치의 아이템 가져오기
@@ -22,6 +32,15 @@ class ContactAdapter(private val mItems: List<Contact>, internal val context: Co
         holder.img.setImageResource(item.aImg) // 프로필 이미지 설정
         holder.name.text = item.aName // 이름 설정
         holder.tel.text = item.aTel // 전화번호 설정
+
+        // 아이템 뷰 클릭 이벤트 처리
+        holder.itemView.setOnClickListener {
+            itemClick?.onClick(it, position) // 아이템 클릭 리스너 호출
+
+            val phoneNumber = item.aTel // 해당 위치의 전화번호 가져오기
+            val dialIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+            context.startActivity(dialIntent)  // 전화 걸기 동작 수행
+        }
     }
 
     // 아이템 개수 반환
