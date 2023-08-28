c106845df70a4219a84846df69163c2350e204bc
ars-yeon <zosel7301@gmail.com>
2023-08-28 20:05:02 +0900

    [FEAT] : (MainActivity) Handle item click event in RecyclerView
    - Implemented item click listener to initiate phone call to the selected contact
    
    Related to #6

--- ClassLevels/ContactListApp/app/src/main/java/com/example/contactlistapp/MainActivity.kt
+++ ClassLevels/ContactListApp/app/src/main/java/com/example/contactlistapp/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.contactlistapp.databinding.ActivityMainBinding
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,5 +22,14 @@ class MainActivity : AppCompatActivity() {
         val adapter = ContactAdapter(dataList, this)
         binding.recyclerView.adapter = adapter
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+
+        // 아이템 클릭을 감지하고 처리
+        adapter.itemClick = object: ContactAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+                val name: String = dataList[position].aName
+                Toast.makeText(this@MainActivity, " $name 님에게 전화를 겁니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
