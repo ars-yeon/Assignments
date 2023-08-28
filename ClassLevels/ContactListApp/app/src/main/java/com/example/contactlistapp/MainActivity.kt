b646c0c0f97d34db3e83181c8b8f9946691d14ba
ars-yeon <zosel7301@gmail.com>
2023-08-28 19:42:41 +0900

    [FEAT] : (MainActivity) Connect RecyclerView and Adapter, Display Dummy Data
    - Connect RecyclerView and Adapter in MainActivity to display data.
    - Load and display dummy data in the RecyclerView.
    
    Related to #6

--- ClassLevels/ContactListApp/app/src/main/java/com/example/contactlistapp/MainActivity.kt
+++ ClassLevels/ContactListApp/app/src/main/java/com/example/contactlistapp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.contactlistapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.contactlistapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val dataList = ContactData.contactData()
+
+        binding.recyclerView.adapter = ContactAdapter(dataList, this)
+
+        val adapter = ContactAdapter(dataList, this)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
