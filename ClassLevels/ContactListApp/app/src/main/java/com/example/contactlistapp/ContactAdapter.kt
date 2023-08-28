d463e788f822449ad61a5c8b45c3bef83025250a
ars-yeon <zosel7301@gmail.com>
2023-08-28 19:29:44 +0900

    [FEAT]: (ContactAdapter) Add functionality
    - onCreateViewHolder
    - onBindViewHolders
    - getItemCount
    
    Related issue: #6

--- ClassLevels/ContactListApp/app/src/main/java/com/example/contactlistapp/ContactAdapter.kt
+++ ClassLevels/ContactListApp/app/src/main/java/com/example/contactlistapp/ContactAdapter.kt
@@ -1,22 +1,32 @@
 package com.example.contactlistapp
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contactlistapp.databinding.ContactItemBinding
 
 class ContactAdapter(private val mItems: List<Contact>, internal val context: Context) :
     RecyclerView.Adapter<ContactAdapter.Holder>() {
+
+    // 뷰 홀더 생성
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactAdapter.Holder {
-        TODO("Not yet implemented")
+        val binding = ContactItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return Holder(binding)
     }
 
+    // 뷰 홀더에 데이터 바인딩
     override fun onBindViewHolder(holder: ContactAdapter.Holder, position: Int) {
-        TODO("Not yet implemented")
+        val item = mItems[position] // 현재 위치의 아이템 가져오기
+
+        holder.img.setImageResource(item.aImg) // 프로필 이미지 설정
+        holder.name.text = item.aName // 이름 설정
+        holder.tel.text = item.aTel // 전화번호 설정
     }
 
+    // 아이템 개수 반환
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return mItems.size
     }
 
     // 뷰 홀더 클래스
