d60cac936cb1267a874a69b475f9606b88b568e8
ars-yeon <zosel7301@gmail.com>
2023-09-02 01:06:49 +0900

    [FEAT] (NewsItem) Add NewsItem data class
    - Added a new data class, NewsItem, which represents a news item in the news reader app.
    - Included properties for the image URL, publication date, title, content, and a bookmark flag.
    - Parcelable for easy transfer between Android components.
    
    Related to #26

---
+++ ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/NewsItem.kt
@@ -0,0 +1,14 @@
+package com.example.newsreaderapp
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.time.LocalDate
+
+@Parcelize
+data class NewsItem(
+    val img: String,
+    val date: LocalDate,
+    val title: String,
+    val content: String,
+    var bookmark: Boolean
+) : Parcelable
