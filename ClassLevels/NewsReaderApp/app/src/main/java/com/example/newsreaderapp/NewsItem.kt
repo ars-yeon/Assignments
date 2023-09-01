0b0acb956c0ff7f112c17ce3afe4a77fef6f75a1
ars-yeon <zosel7301@gmail.com>
2023-09-02 02:15:04 +0900

    [REFACTOR] (NewsItem) Update date property to use LocalDateTime
    - Updated the 'date' property in the NewsItem data class to use LocalDateTime for more precise date and time representation.
    
    Related to #26

--- ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/NewsItem.kt
+++ ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/NewsItem.kt
@@ -2,13 +2,13 @@ package com.example.newsreaderapp
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Parcelize
 data class NewsItem(
     val img: String,
     val agency:String,
-    val date: LocalDate,
+    val date: LocalDateTime,
     val title: String,
     val content: String,
     var bookmark: Boolean
