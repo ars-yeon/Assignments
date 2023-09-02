cd0ed8d5940e160be330072bcddde3630195f705
ars-yeon <zosel7301@gmail.com>
2023-09-03 00:19:19 +0900

    [FEAT] (TitleFragment) Initialize RecyclerView
    - Added code for initializing RecyclerView
    - Removed unnecessary comments
    
    Related to #26

--- ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/TitleFragment.kt
+++ ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/TitleFragment.kt
@@ -5,21 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class TitleFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    private val mItems: List<NewsItem> = DummyData.dummyItem()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,20 +29,18 @@ class TitleFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        val view = inflater.inflate(R.layout.fragment_title, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerView)
+        val adapter = NewsAdapter(mItems)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TitleFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             TitleFragment().apply {
