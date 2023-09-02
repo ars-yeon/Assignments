fdc75cd956a097a8113f3f8c246dcc35535fe181
ars-yeon <zosel7301@gmail.com>
2023-09-03 00:01:23 +0900

    [FEAT] (NewsItem) Modify Image Data Type and Add Formatted Date Property
    - Changed the data type of the 'img' property from String to Int.
    - Added a new property called 'formattedDate' to provide functionality for formatting dates in a specified format.
    
    Related to #26

--- ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/NewsItem.kt
+++ ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/NewsItem.kt
@@ -3,13 +3,20 @@ package com.example.newsreaderapp
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Parcelize
 data class NewsItem(
-    val img: String,
+    val img: Int,
     val agency:String,
     val date: LocalDateTime,
     val title: String,
     val content: String,
     var bookmark: Boolean
-) : Parcelable
+) : Parcelable {
+    val formattedDate: String
+        get() {
+            val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm")
+            return date.format(formatter)
+        }
+}
