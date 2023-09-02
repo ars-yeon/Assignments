7a1206eb664ae4e2cfa5cb7bb3049c9cc0905e8b
ars-yeon <zosel7301@gmail.com>
2023-09-03 00:24:15 +0900

    [FEAT] (MainActivity) Passed dummy data to TitleFragment and added the fragment to the screen
    - Added code to pass dummy data to the TitleFragment.
    - Added the TitleFragment to the fragment_container.
    
    Related to #26

--- ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/MainActivity.kt
+++ ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/MainActivity.kt
@@ -7,5 +7,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 더미데이터 연결
+        val dummyData: List<NewsItem> = DummyData.dummyItem()
+
+        //TitleFragment에 더미데이터 전달
+        val titleFragment = TitleFragment()
+        val bundle = Bundle()
+        bundle.putParcelableArrayList("newsItems", ArrayList(dummyData))
+        titleFragment.arguments = bundle
+
+// TitleFragment를 화면에 추가
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, titleFragment)
+            .commit()
     }
 }
