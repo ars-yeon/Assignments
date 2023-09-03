6f303c33150d491f1f6e9219033babf76d46bf70
ars-yeon <zosel7301@gmail.com>
2023-09-03 23:53:04 +0900

    [FEAT] (DetailFragment) Data Binding and Back Button Behavior
    - Utilized data binding to display data received from TitleFragment on the screen.
    - Modified the click behavior of the back button to navigate to the TitleFragment.
    
    Related to # 28

--- ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/DetailFragment.kt
+++ ClassLevels/NewsReaderApp/app/src/main/java/com/example/newsreaderapp/DetailFragment.kt
@@ -5,55 +5,65 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.newsreaderapp.databinding.FragmentDetailBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@Suppress("DEPRECATION")
 class DetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var binding: FragmentDetailBinding
+//    private var bookmark = false
+
+    private var articleDetail: String? = null
+    /*private val newsPosition: Int by lazy {
+        arguments?.getInt(Constants.NEWS_INDEX, 0) ?: 0
+    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            articleDetail = it.getString("articleDetail")
         }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+    ): View {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        val ivImg = binding.ivImg
+        val tvagency = binding.tvAgency
+        val tvDate = binding.tvDate
+        val tvTitle = binding.tvTitle
+        val tvConent = binding.tvContent
+        arguments?.let { it ->
+            val clickedNewsItem = it.getParcelable<NewsItem>(Constants.NEWS_OBJECT)
+            clickedNewsItem?.let {
+                ivImg.setImageResource(it.img)
+                tvagency.text = it.agency
+                tvDate.text = it.formattedDate
+                tvTitle.text = it.title
+                tvConent.text = it.content
+            }
+        }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.ivBack.setOnClickListener {
+            val titleFragment = TitleFragment()
+            requireActivity().supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, titleFragment)
+                .commit()
+        }
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance(newsItem: NewsItem): DetailFragment {
+            val fragment = DetailFragment()
+            val bundle = Bundle()
+            bundle.putParcelable("newsItem", newsItem)
+            fragment.arguments = bundle
+            return fragment
+        }
     }
 }
