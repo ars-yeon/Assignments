6c5fe5d9f77be7adff2a2d83bafe2189420a8b47
ars-yeon <zosel7301@gmail.com>
2023-09-18 13:04:04 +0900

    [FEAT] : ⚙️(MainActivity): Implement MainActivity
    - Created the MainActivity class, which serves as the main entry point of the app.
    - Set up the main layout using data binding.
    - Initialized the ViewPager2 for fragment navigation and the BottomNavigationView for switching between app sections (Search and Collection).
    - Implemented navigation logic to switch between sections when bottom navigation items are selected.

--- Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/main/MainActivity.kt
+++ Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/main/MainActivity.kt
@@ -3,10 +3,35 @@ package com.example.imagesearchapp.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.imagesearchapp.R
+import com.example.imagesearchapp.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { MainActivityBinding.inflate(layoutInflater) }
+    private val viewPager by lazy { binding.fragmentContainer }
+    private val bottomNav by lazy {binding.bottomNavi}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(binding.root)
+
+        initViewPager()
+        initNavigationBar()
+    }
+
+    private fun initViewPager() {
+        val adapter = PagerAdapter(this)
+        viewPager.adapter = adapter
+    }
+
+    private fun initNavigationBar() {
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navi_search -> viewPager.currentItem = 0
+                R.id.navi_collection -> viewPager.currentItem = 1
+            }
+            true
+        }
+        bottomNav.selectedItemId = R.id.navi_search
     }
 }
