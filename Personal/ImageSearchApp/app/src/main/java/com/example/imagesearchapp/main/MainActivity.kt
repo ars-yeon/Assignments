660982d6336c690371b4e36ca21640888a83c0a8
ars-yeon <zosel7301@gmail.com>
2023-09-18 11:55:17 +0900

    [RENAME] : ✏️(MainActivity): Move MainActivity.kt to the 'main' directory

--- Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/main/MainActivity.kt
+++ Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/main/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.imagesearchapp
+package com.example.imagesearchapp.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.imagesearchapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
     }
 }
