6ac067980d196c36953bd95d3579ba1631bd35f9
ars-yeon <zosel7301@gmail.com>
2023-09-19 19:40:30 +0900

    [FEAT] : ⚙️ (CollectionFragment) : Connected SharedViewModel and Adapter
    - Connected SharedViewModel to observe the list of bookmarked items and pass the updated list to CollectionAdapter.
    - Updated CollectionAdapter to use SharedViewModel for managing data.

--- Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/collection/CollectionFragment.kt
+++ Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/collection/CollectionFragment.kt
@@ -5,6 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.imagesearchapp.data.KakaoImage
+import com.example.imagesearchapp.data.SharedViewModel
 import com.example.imagesearchapp.databinding.CollectionFragmentBinding
 
 
@@ -12,12 +18,33 @@ class CollectionFragment : Fragment() {
 
     private var _binding: CollectionFragmentBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: CollectionAdapter
+    private lateinit var viewModel: SharedViewModel
+    private val collectionItems = mutableListOf<KakaoImage>()
+    private val sharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = CollectionFragmentBinding.inflate(inflater, container, false)
+
+        val recyclerView = binding.collectionRv
+
+        viewModel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
+        adapter = CollectionAdapter(viewModel)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+
+        sharedViewModel.getBookmarkedItems().observe(viewLifecycleOwner, Observer { bookmarkedItems ->
+            adapter.submitList(bookmarkedItems.toList())
+        })
+
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
