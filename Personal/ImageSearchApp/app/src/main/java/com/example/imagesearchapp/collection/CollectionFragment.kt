f02c636f70d400700d45d4061ec20cee79f05b00
ars-yeon <zosel7301@gmail.com>
2023-09-20 15:17:20 +0900

    [FIX] : 🔧(CollectionFragment): Connect bookmark feature via SharedViewModel
    - Simplified the CollectionFragment code for improved readability.
    - Directly connected the bookmark functionality to the 'bookmarkedItems' LiveData in SharedViewModel for data synchronization.

--- Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/collection/CollectionFragment.kt
+++ Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/collection/CollectionFragment.kt
@@ -7,9 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.imagesearchapp.data.KakaoImage
 import com.example.imagesearchapp.data.SharedViewModel
 import com.example.imagesearchapp.databinding.CollectionFragmentBinding
 
@@ -19,8 +17,6 @@ class CollectionFragment : Fragment() {
     private var _binding: CollectionFragmentBinding? = null
     private val binding get() = _binding!!
     private lateinit var adapter: CollectionAdapter
-    private lateinit var viewModel: SharedViewModel
-    private val collectionItems = mutableListOf<KakaoImage>()
     private val sharedViewModel: SharedViewModel by activityViewModels()
 
     override fun onCreateView(
@@ -31,12 +27,11 @@ class CollectionFragment : Fragment() {
 
         val recyclerView = binding.collectionRv
 
-        viewModel = ViewModelProvider(requireActivity())[SharedViewModel::class.java]
-        adapter = CollectionAdapter(viewModel)
+        adapter = CollectionAdapter(sharedViewModel)
         recyclerView.adapter = adapter
         recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
 
-        sharedViewModel.getBookmarkedItems().observe(viewLifecycleOwner, Observer { bookmarkedItems ->
+        sharedViewModel.bookmarkedItems.observe(viewLifecycleOwner, Observer { bookmarkedItems ->
             adapter.submitList(bookmarkedItems.toList())
         })
 
