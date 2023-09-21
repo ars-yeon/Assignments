4783dcfb760caf39f5efb5d28f8ff2b147d9ef3f
ars-yeon <zosel7301@gmail.com>
2023-09-21 17:23:00 +0900

    [FEAT] : ⚙️(SharedViewModel): Add Bookmarking and Storage Features
    - Modified the toggleBookmark method to accept a Context parameter, ensuring that bookmarks are also stored locally when toggled.
    - Introduced the saveBookmarks method to save bookmarked items to local storage.
    - Implemented the loadBookmarks method to retrieve bookmarked items from local storage.

--- Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/data/SharedViewModel.kt
+++ Personal/ImageSearchApp/app/src/main/java/com/example/imagesearchapp/data/SharedViewModel.kt
@@ -1,9 +1,12 @@
 package com.example.imagesearchapp.data
 
+import android.content.Context
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import retrofit2.Call
 
 class SharedViewModel : ViewModel() {
@@ -16,13 +19,34 @@ class SharedViewModel : ViewModel() {
     private val _bookmarkedItems = MutableLiveData<Set<KakaoImage>>(setOf())
     val bookmarkedItems: LiveData<Set<KakaoImage>> = _bookmarkedItems
 
-    fun toggleBookmark(item: KakaoImage) {
+    fun toggleBookmark(context: Context, item: KakaoImage) {
         val currentItems = _bookmarkedItems.value?.toMutableSet() ?: mutableSetOf()
 
         if (item in currentItems) currentItems.remove(item)
         else currentItems.add(item)
 
         _bookmarkedItems.value = currentItems.toSet()
+        saveBookmarks(context)
+    }
+
+    private fun saveBookmarks(context: Context) {
+        val pref = context.getSharedPreferences("bookmark_prefs", Context.MODE_PRIVATE)
+        val editor = pref.edit()
+        val bookmarkedItemsSet = _bookmarkedItems.value ?: setOf()
+        val bookmarkedItemsJson = Gson().toJson(bookmarkedItemsSet.toList())
+        editor.putString("bookmarked_items", bookmarkedItemsJson)
+        editor.apply()
+    }
+
+    fun loadBookmarks(context: Context) {
+        val pref = context.getSharedPreferences("bookmark_prefs", Context.MODE_PRIVATE)
+        val bookmarkedItemsJson = pref.getString("bookmarked_items", null)
+
+        if (bookmarkedItemsJson != null) {
+            val typeToken = object : TypeToken<Set<KakaoImage>>() {}.type
+            val bookmarkedItemsSet = Gson().fromJson<Set<KakaoImage>>(bookmarkedItemsJson, typeToken)
+            _bookmarkedItems.value = bookmarkedItemsSet
+        }
     }
 
     init {
