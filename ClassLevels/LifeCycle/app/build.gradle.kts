29e8b77e582f1d84a9dfc7289621b6c83d26365e
ars-yeon <zosel7301@gmail.com>
2023-09-22 19:33:25 +0900

    [FEAT] : ⚙️(build.gradle): Updates Compile Version and Library
    - Upgraded compileSdk version to 34.
    - Enabled View Binding.
    - Updated core-ktx dependency version.
    - Added fragment-ktx dependency.

--- ClassLevels/LifeCycle/app/build.gradle.kts
+++ ClassLevels/LifeCycle/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.lifecycle"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.lifecycle"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -33,14 +33,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
