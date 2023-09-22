61eedf5f1ba0f6b2a73b71a19834a000fe0e7d1b
ars-yeon <zosel7301@gmail.com>
2023-09-22 22:35:21 +0900

    [FEAT] : ⚙️(SubActivity): Add Lifecycle-specific Logging
    - Updated the SubActivity class to record logs for each activity lifecycle event (onCreate, onStart, onResume, onPause, onStop, onDestroy).
    - Utilized the 'sub' constant from the Constants class as the log tag.
    - Logs have been implemented to indicate the occurrence of each activity lifecycle event in Logcat.

--- ClassLevels/LifeCycle/app/src/main/java/com/example/lifecycle/SubActivity.kt
+++ ClassLevels/LifeCycle/app/src/main/java/com/example/lifecycle/SubActivity.kt
@@ -2,10 +2,38 @@ package com.example.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.lifecycle.Constants.sub
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+        Log.d(sub, "onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(sub, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(sub, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(sub, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(sub, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(sub, "onDestroy called")
     }
 }
