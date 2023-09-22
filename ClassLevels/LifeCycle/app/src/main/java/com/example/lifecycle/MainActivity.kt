a1550d5847a519fb16f01a587eb485b511111e27
ars-yeon <zosel7301@gmail.com>
2023-09-22 23:12:54 +0900

    [FEAT] : ⚙️(MainActivity): Update UI and Event Handling
    - Implemented layout and view binding using ActivityMainBinding.
    - Added buttons (btnSubActivity and btnFirstFragment) for navigating to SubActivity and FirstFragment, with corresponding click event handling.
    - Implemented event handling for buttons (btnToastShort, btnToastLong) that display Toast messages.
    - Introduced a custom Context extension function for easy and concise Toast message display.

--- ClassLevels/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ ClassLevels/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -1,15 +1,49 @@
 package com.example.lifecycle
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.commit
 import com.example.lifecycle.Constants.main
+import com.example.lifecycle.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
         Log.d(main, "onCreate called")
+
+        val btnSub = binding.btnSubActivity
+        val btnFrag = binding.btnFirstFragment
+        val btnToastShort = binding.btnToastShort
+        val btnToastLong = binding.btnToastLong
+
+        btnSub.setOnClickListener {
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnFrag.setOnClickListener {
+            supportFragmentManager.commit {
+                replace(R.id.fragment_container, FirstFragment())
+                addToBackStack(null)
+            }
+        }
+
+        btnToastShort.setOnClickListener {
+            Toast("This is Toast Message.")
+
+        }
+        btnToastLong.setOnClickListener {
+            Toast("This Toast Message will be displayed for a longer time.", Toast.LENGTH_LONG)
+        }
     }
 
     override fun onStart() {
@@ -35,5 +69,9 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(main, "onDestroy called")
+    }
+
+    private fun Context.Toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
+        Toast.makeText(this, message, duration).show()
     }
 }
