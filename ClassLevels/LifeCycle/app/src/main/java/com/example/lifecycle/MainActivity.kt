369c84aa6bd9afe1bf1ec551f09abcd54d6bd844
ars-yeon <zosel7301@gmail.com>
2023-09-22 22:34:41 +0900

    [FEAT] : ⚙️(MainActivity): Add Lifecycle-specific Logging
    - Updated the MainActivity class to record logs for each activity lifecycle event (onCreate, onStart, onResume, onPause, onStop, onDestroy).
    - Utilized the 'main' constant from the Constants class as the log tag.
    - Logs have been implemented to indicate the occurrence of each activity lifecycle event in Logcat.

--- ClassLevels/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ ClassLevels/LifeCycle/app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.lifecycle.Constants.main
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(main, "onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(main, "onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(main, "onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(main, "onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(main, "onStop called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(main, "onDestroy called")
     }
 }
